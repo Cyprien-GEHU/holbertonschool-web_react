378a40504a639c19c0bbb104a6da69895efc4f27
Cyprien <cypriengehu@example.com>
2026-01-21 14:30:02 +0100

    feat: task 2 - Adding CSS & Images

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+   mode: "production",
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        }
+      },
+   ],
+  },
+};
