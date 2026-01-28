8a30241a7e514e72daadca04aaf0a4c790e86e30
Cyprien <cypriengehu@example.com>
2026-01-28 15:50:25 +0100

    fix: task 3 - correction on password (htmlfor)

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -17,10 +17,13 @@ function App() {
       </div>
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
+
         <label htmlFor='email'>Email:</label>
         <input type="email" id="email"/>
-        <label htmlFor='passworsd'>Password:</label>
+
+        <label htmlFor='password'>Password:</label>
         <input type="password" id="password"/>
+        
         <button>OK</button>
       </div>
       <div className='App-footer'>
