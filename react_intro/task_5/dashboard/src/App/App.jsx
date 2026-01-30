37a82cc89dd37bd83274e4150ed75359266bde0e
Cyprien <cypriengehu@example.com>
2026-01-30 15:30:16 +0100

    feat: task 9 -Reorganize the files

---
+++ react_intro/task_5/dashboard/src/App/App.jsx
@@ -0,0 +1,36 @@
+import './App.css';
+import '../Notifications/Notifications.css'
+import holbertonLogo from '../assets/holberton-logo.jpg';
+import { getCurrentYear, getFooterCopy } from '../utils/utils.js'
+import Notifications from '../Notifications/Notifications.jsx';
+
+
+function App() {
+  return (
+    <>
+      <div className='root-notifications'>
+        <Notifications />
+      </div>
+      <div className='App-header'>
+        <img src={holbertonLogo} alt="holberton logo"/>
+        <h1>School dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+
+        <label htmlFor='email'>Email:</label>
+        <input type="email" id="email"/>
+
+        <label htmlFor='password'>Password:</label>
+        <input type="password" id="password"/>
+        
+        <button>OK</button>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,33 @@
+import { getLatestNotification } from "../utils/utils";
+import closeButton from "../assets/close-button.png";
+
+function Notifications() {
+  function closeClick() {
+    console.log("Close button has been clicked");
+  }
+  return (
+    <>
+      <div className="notification-items">
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li
+            data-priority="urgent"
+            dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+          ></li>
+        </ul>
+        <button aria-label="Close" onClick={closeClick}>
+          <img
+            src={closeButton}
+            alt="close button"
+            width="12px"
+            height="12px"
+          />
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Notifications;
