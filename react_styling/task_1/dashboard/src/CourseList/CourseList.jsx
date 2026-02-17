eba3362d88496037c52ade103ddd60b856989c9b
Cyprien <cypriengehu@example.com>
2026-02-17 13:11:08 +0100

    fix:  React styling task 1 -  try something else

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -1,41 +1,43 @@
 import CourseListRow from "./CourseListRow";
 
 function CourseList({ courses = [] }) {
-  if (courses.length === 0) {
-    return (
-      <div className="w-4/5 mx-auto mt-8">
-        <table className="w-full border border-collapse">
+  return (
+    <div className="w-[85%] mx-auto mt-8">
+      <table className="w-full border-collapse border border-gray-400">
+        {courses.length === 0 ? (
           <tbody>
             <CourseListRow
               isHeader={true}
               textFirstCell="No course available yet"
+              textSecondCell=""
             />
           </tbody>
-        </table>
-      </div>
-    );
-  }
-  return (
-    <div className="w-4/5 mx-auto mt-8">
-      <table className="w-full border border-collapse">
-        <thead>
-          <CourseListRow textFirstCell="Available courses" isHeader={true} />
-          <CourseListRow
-            isHeader={true}
-            textFirstCell="Course name"
-            textSecondCell="Credit"
-          />
-        </thead>
-        <tbody>
-          {courses.map((course) => (
-            <CourseListRow
-              key={course.id}
-              textFirstCell={course.name}
-              textSecondCell={course.credit}
-              isHeader={false}
-            />
-          ))}
-        </tbody>
+        ) : (
+          <>
+            <thead>
+              <CourseListRow
+                isHeader={true}
+                textFirstCell="Available courses"
+                textSecondCell=""
+              />
+              <CourseListRow
+                isHeader={true}
+                textFirstCell="Course name"
+                textSecondCell="Credit"
+              />
+            </thead>
+            <tbody>
+              {courses.map((course) => (
+                <CourseListRow
+                  key={course.id}
+                  isHeader={false}
+                  textFirstCell={course.name}
+                  textSecondCell={course.credit}
+                />
+              ))}
+            </tbody>
+          </>
+        )}
       </table>
     </div>
   );
