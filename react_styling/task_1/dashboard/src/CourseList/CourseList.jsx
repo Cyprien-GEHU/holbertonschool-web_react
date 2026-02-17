ab721881862f42c6e8e503ac96709b615ec32141
Cyprien <cypriengehu@example.com>
2026-02-17 13:24:49 +0100

    fix:  React styling task 1 -  try something else

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -4,16 +4,14 @@ function CourseList({ courses = [] }) {
   return (
     <div className="w-4/5 mx-auto">
       <table className="w-full border-collapse">
-        {courses.length === 0 ? (
-          <thead>
+        <thead>
+          {courses.length === 0 ? (
             <CourseListRow
               isHeader={true}
               textFirstCell="No course available yet"
             />
-          </thead>
-        ) : (
-          <>
-            <thead>
+          ) : (
+            <>
               <CourseListRow
                 isHeader={true}
                 textFirstCell="Available courses"
@@ -23,19 +21,19 @@ function CourseList({ courses = [] }) {
                 textFirstCell="Course name"
                 textSecondCell="Credit"
               />
-            </thead>
-            <tbody>
-              {courses.map((course) => (
-                <CourseListRow
-                  key={course.id}
-                  isHeader={false}
-                  textFirstCell={course.name}
-                  textSecondCell={course.credit}
-                />
-              ))}
-            </tbody>
-          </>
-        )}
+            </>
+          )}
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <CourseListRow
+              key={course.id}
+              isHeader={false}
+              textFirstCell={course.name}
+              textSecondCell={course.credit}
+            />
+          ))}
+        </tbody>
       </table>
     </div>
   );
