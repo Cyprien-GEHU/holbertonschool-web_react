021e369596f2436b2b2f998cfbb3dc24246a1fea
Cyprien <cypriengehu@example.com>
2026-02-17 13:14:50 +0100

    fix:  React styling task 1 -  try something else

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -2,23 +2,21 @@ import CourseListRow from "./CourseListRow";
 
 function CourseList({ courses = [] }) {
   return (
-    <div className="w-[85%] mx-auto mt-8">
-      <table className="w-full border-collapse border border-gray-400">
+    <div className="w-4/5 mx-auto">
+      <table className="w-full border-collapse">
         {courses.length === 0 ? (
-          <tbody>
+          <thead>
             <CourseListRow
               isHeader={true}
               textFirstCell="No course available yet"
-              textSecondCell=""
             />
-          </tbody>
+          </thead>
         ) : (
           <>
             <thead>
               <CourseListRow
                 isHeader={true}
                 textFirstCell="Available courses"
-                textSecondCell=""
               />
               <CourseListRow
                 isHeader={true}
