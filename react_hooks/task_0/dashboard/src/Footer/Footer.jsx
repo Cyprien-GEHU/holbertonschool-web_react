9bfad883d9e1c6ef5e98b7ace0453bc07d96c1b6
Cyprien <cypriengehu@example.com>
2026-03-05 13:42:05 +0100

    feat:  React hooks task 1 - Update Footer Component

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,28 +1,23 @@
 import { getCurrentYear, getFooterCopy } from "../utils/utils";
 import AppContext from "../Context/context";
+import { useContext } from "react";
 
 function Footer({ isIndex = false }) {
   const currentYear = getCurrentYear();
   const footerCopy = getFooterCopy(isIndex);
+  const {user} = useContext(AppContext)
   return (
-    <AppContext.Consumer>
-      {(valueContext) => {
-        const { user } = valueContext;
-        return (
-          <footer className="App-footer flex justify-center items-center border-t-4 border-[color:var(--main-color)] w-full mt-auto py-2">
-            <p className="italic text-xl p-1 text-center max-[520px]:text-lg max-[520px]:p-0 max-[450px]:text-[16px] max-[375px]:text-[15px]">
-              Copyright {currentYear} - {footerCopy}
-            </p>
+    <footer className="App-footer flex justify-center items-center border-t-4 border-[color:var(--main-color)] w-full mt-auto py-2">
+      <p className="italic text-xl p-1 text-center max-[520px]:text-lg max-[520px]:p-0 max-[450px]:text-[16px] max-[375px]:text-[15px]">
+        Copyright {currentYear} - {footerCopy}
+      </p>
 
-            {user && user.isLoggedIn && (
-              <p>
-                <a href="#">Contact us</a>
-              </p>
-            )}
-          </footer>
-        );
-      }}
-    </AppContext.Consumer>
+      {user && user.isLoggedIn && (
+        <p>
+          <a href="#">Contact us</a>
+        </p>
+      )}
+    </footer>
   );
 }
 
