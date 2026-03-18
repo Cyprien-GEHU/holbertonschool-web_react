ce44f202dc77c66a45c0b3b3891502caa01ac304
Cyprien <cypriengehu@example.com>
2026-03-18 10:13:20 +0100

    feat:  React Redux (part 1) task 6 -  Synchronize Header with Redux store

---
+++ react_redux-part1/task_3/dashboard/src/features/notifications/notificationsSlice.js
@@ -0,0 +1,62 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { getLatestNotification } from "../../utils/utils";
+
+const initialState = {
+  notifications: [],
+  displayDrawer: true,
+};
+
+const API_BASE_URL = "http://localhost:5173";
+const ENDPOINTS = {
+  notifications: `${API_BASE_URL}/notifications.json`,
+};
+
+export const fetchNotifications = createAsyncThunk(
+  "notifications/fetchNotifications",
+  async () => {
+    const res = await axios.get(ENDPOINTS.notifications);
+    const data = res.data.notifications || res.data;
+
+    const updateFetch = data.map((notification) => {
+      if (notification.id === 3) {
+        return {
+          ...notification,
+          html: { __html: getLatestNotification() },
+        };
+      }
+      return notification;
+    });
+    return updateFetch;
+  },
+);
+
+const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState,
+  reducers: {
+    markNotificationAsRead: (state, action) => {
+      const notifId = action.payload;
+      console.log(`Marking notification ${notifId} as read`);
+      state.notifications = state.notifications.filter(
+        (notif) => notif.id !== notifId,
+      );
+    },
+    showDrawer: (state) => {
+      state.displayDrawer = true;
+    },
+    hideDrawer: (state) => {
+      state.displayDrawer = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.notifications = action.payload;
+    });
+  },
+});
+
+export const { markNotificationAsRead, showDrawer, hideDrawer } =
+  notificationsSlice.actions;
+
+export default notificationsSlice.reducer;
