7cb0bb77ce01af3c943e1d6f7d0c6458bae7e9cc
Cyprien <cypriengehu@example.com>
2026-03-18 10:59:13 +0100

    feat:  React Redux (part 1) task 7 -  Synchronize Footer with Redux store

---
+++ react_redux-part1/task_4/dashboard/src/features/courses/coursesSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { logout } from "../auth/authSlice";
+
+const initialState = {
+  courses: [],
+};
+
+const API_BASE_URL = "http://localhost:5173";
+const ENDPOINTS = {
+  notifications: `${API_BASE_URL}/courses.json`,
+};
+
+export const fetchCourses = createAsyncThunk(
+  "courses/fetchCourses",
+  async () => {
+    const res = await axios.get(ENDPOINTS.courses);
+    const data = res.data.courses || res.data;
+    const dataCourses = data.map((course) => {
+      return course;
+    });
+    return dataCourses;
+  },
+);
+
+const coursesSlice = createSlice({
+  name: "courses",
+  initialState,
+  reducers: {},
+  extraReducers: (build) => {
+    build
+      .addCase(fetchCourses.fulfilled, (state, action) => {
+        state.courses = action.payload;
+      })
+      .addCase(fetchCourses.rejected, (state) => {
+        return state;
+      })
+      .addCase(logout, () => {
+        return initialState;
+      });
+  },
+});
+
+export default coursesSlice.reducer;
