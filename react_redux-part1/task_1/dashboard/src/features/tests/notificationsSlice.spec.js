80d089fd4b23e87f4998ea59fce1555ed68e2455
Cyprien <cypriengehu@example.com>
2026-03-18 09:49:57 +0100

    feat:  React Redux (part 1) task 3 - Courses Slice

--- react_redux-part1/task_1/dashboard/src/features/tests/notificationsSlice.spec.js
+++ react_redux-part1/task_1/dashboard/src/features/tests/notificationsSlice.spec.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mockAxios from "jest-mock-axios";
-import axios from "axios";
 import notificationsReducer, {
   fetchNotifications,
   markNotificationAsRead,
