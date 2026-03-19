ae8443c31b6473bf2105981288b1e66fdfdcb785
Cyprien <cypriengehu@example.com>
2026-03-19 14:24:59 +0100

    fix:  React Redux (part 2) task 2 -    eslint

--- react_redux-part2/task_2/dashboard/src/features/tests/coursesSlice.spec.js
+++ react_redux-part2/task_2/dashboard/src/features/tests/coursesSlice.spec.js
@@ -35,10 +35,22 @@ describe("all test for coursesSlice", () => {
 
     const stateStore = store.getState();
     expect(stateStore.courses).toHaveLength(4);
-    expect(stateStore.courses[0]).toEqual({ ...mockCourses[0], "isSelected": false});
-    expect(stateStore.courses[1]).toEqual({ ...mockCourses[1], "isSelected": false});
-    expect(stateStore.courses[2]).toEqual({ ...mockCourses[2], "isSelected": false});
-    expect(stateStore.courses[3]).toEqual({ ...mockCourses[3],"isSelected": false});
+    expect(stateStore.courses[0]).toEqual({
+      ...mockCourses[0],
+      isSelected: false,
+    });
+    expect(stateStore.courses[1]).toEqual({
+      ...mockCourses[1],
+      isSelected: false,
+    });
+    expect(stateStore.courses[2]).toEqual({
+      ...mockCourses[2],
+      isSelected: false,
+    });
+    expect(stateStore.courses[3]).toEqual({
+      ...mockCourses[3],
+      isSelected: false,
+    });
   });
 
   it("render a resets the courses array to empty whenever the logout action is dispatched", () => {
